c821ece395bec56e7fe399172ef9a397c9ca9fae
rammalali <ali,rammal.4@hotmail.com>
2023-04-19 13:21:23 +0300

    docker added

--- fastAPI/app.py
+++ fastAPI/app.py
@@ -1,38 +1,27 @@
-from typing import Union
+from typing import Union, Any
 import os
+import shutil
+import json
+import base64
+import uuid
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse, FileResponse
 from pydantic import BaseModel
-import json
-import base64
-import onnxruntime
-import shutil
-import torch
 from PIL import Image
 from io import BytesIO
-from torchvision import transforms
-from typing import Any
-import uuid
 
 from detect import run
 
-
-# load the model
 model_path = "runs/train/exp/weights/yolov5.onnx"
-session = onnxruntime.InferenceSession(model_path)
-
-
+conf_threshold = 0.65
 app = FastAPI()
 
-
-class my_json(BaseModel):
+class ImageJson(BaseModel):
     label: Any
-    # label_path: str
 
-
-class BASE64_input(BaseModel):
+class BASE64Input(BaseModel):
     image_path: str
-
 
 def get_label_path(image_path, format="json"):
     base_path = os.path.dirname(os.path.dirname(image_path))
@@ -42,13 +31,8 @@ def get_label_path(image_path, format="json"):
     label_path = os.path.join(base_path, "labels", format, label_filename)
     return label_path
 
-
 def remove_dir(dir_path="runs/detect/exp"):
-    # Remove the directory (if it's empty)
-    dir_path = "runs/detect/exp"
-    # Check if the directory exists
     if os.path.exists(dir_path):
-        # Remove the directory and its contents
         try:
             shutil.rmtree(dir_path)
             print(f"Directory {dir_path} removed successfully.")
@@ -57,8 +41,7 @@ def remove_dir(dir_path="runs/detect/exp"):
     else:
         print(f"Directory {dir_path} does not exist.")
 
-
-def extract_coordinates(self, json_path):
+def extract_coordinates(json_path):
     with open(json_path, "rb") as f:
         data = json.load(f)
     coordinates = []
@@ -86,26 +69,14 @@ def extract_coordinates(self, json_path):
             )
     return coordinates
 
-
 def base64_to_image(base64_str, save_path="images_from_base64\image.png", format="PNG"):
     img_data = base64.b64decode(base64_str)
     img = Image.open(BytesIO(img_data))
     img.save(save_path, format=format)
     return img
 
-
-def txt_to_json(txt):
-    # this to convert it back from txt to json
-    # x_center = (left + right) / (2 * image_width)
-    # y_center = (top + bottom) / (2 * image_height)
-    # width = (right - left) / image_width
-    # height = (bottom - top) / image_height
-
-    image_width = 1280
-    image_height = 720
-
+def txt_to_json(txt, image_width=1280, image_height=720):
     lines = txt.strip().split("\n")
-    print(lines)
     json_objects = []
 
     for i, line in enumerate(lines):
@@ -151,17 +122,15 @@ def get_labels(model_name: str):
     else:
         return JSONResponse(status_code=404, content={"detail": "Model not found"})
 
-
-@app.post("/{model_name}/json/", response_model=my_json)
-def get_json(item: BASE64_input, model_name: str):
+@app.post("/{model_name}/json/", response_model=ImageJson)
+def get_json(item: BASE64Input, model_name: str):
     remove_dir("runs/detect/exp")
-
     base64_to_image(item.image_path)
-    # os.rmtree("runs/detect/exp5")
+
     if model_name == "yolov5":
         run(
-            weights="runs/train/exp/weights/best.onnx",
-            conf_thres=0.25,
+            weights="runs/train/exp/weights/yolov5.onnx",
+            conf_thres=conf_threshold,
             source="images_from_base64",
             save_txt=True,
             save_conf=True,
@@ -170,14 +139,6 @@ def get_json(item: BASE64_input, model_name: str):
 
     elif model_name == "faster-rcnn":
         return {"label": "model not done yet!"}
-        # run(
-        #     weights="runs/train/exp/weights/best.onnx",
-        #     conf_thres=0.25,
-        #     source="images_from_base64",
-        #     save_txt=True,
-        #     save_conf=True,
-        #     name="exp",
-        # )
 
     else:
         raise HTTPException(status_code=404, detail="Model Not Found !")
@@ -190,88 +151,62 @@ def get_json(item: BASE64_input, model_name: str):
     remove_dir("runs/detect/exp")
     return {"label": label}
 
-
 @app.post("/{model_name}/predict-box/")
-async def predict_box(model_name: str, file: UploadFile = File(...)):  # async for await
+async def predict_box(model_name: str, file: UploadFile = File(...)):
     file.filename = f"image.png"
     contents = await file.read()
-
-    # Remove the directory (if it's empty)
     remove_dir("runs/detect/exp")
 
-    # Save the image to disk
     with open(f"data/images/{file.filename}", "wb") as f:
         f.write(contents)
 
-    if model_name == "faster-rcnn":
-        return {"label": "model not done yet!"}
-        # run(
-        #     weights="runs/train/exp/weights/best.onnx",
-        #     conf_thres=0.25,
-        #     source="data/images",
-        #     save_txt=True,
-        #     save_conf=True,
-        #     name="exp",
-        # )
-    elif model_name == "faster-rcnn":
+    if model_name == "yolov5":
         run(
-            weights="runs/train/exp/weights/best.onnx",
-            conf_thres=0.25,
+            weights="runs/train/exp/weights/yolov5.onnx",
+            conf_thres=conf_threshold,
             source="data/images",
             save_txt=True,
             save_conf=True,
             name="exp",
         )
+
+    elif model_name == "faster-rcnn":
+        return {"label": "model not done yet!"}
 
     else:
         raise HTTPException(status_code=404, detail="Model Not Found !")
 
     return FileResponse("runs/detect/exp/image.png")
 
-
-@app.post("/{model_name}/predict-json/", response_model=my_json)
-async def predict_json(
-    model_name: str, file: UploadFile = File(...)
-):  # async for await
+@app.post("/{model_name}/predict-json/", response_model=ImageJson)
+async def predict_json(model_name: str, file: UploadFile = File(...)):
     file.filename = f"image.png"
     contents = await file.read()
-
-    # Remove the directory (if it's empty)
     remove_dir("runs/detect/exp")
 
-    # Save the image to disk
     with open(f"data/images/{file.filename}", "wb") as f:
         f.write(contents)
 
-    if model_name == "faster-rcnn":
-        return {"label": "model not done yet!"}
-        # run(
-        #     weights="runs/train/exp/weights/best.onnx",
-        #     conf_thres=0.25,
-        #     source="data/images",
-        #     save_txt=True,
-        #     save_conf=True,
-        #     name="exp",
-        # )
-    elif model_name == "faster-rcnn":
+    if model_name == "yolov5":
         run(
-            weights="runs/train/exp/weights/best.onnx",
-            conf_thres=0.25,
+            weights="runs/train/exp/weights/yolov5.onnx",
+            conf_thres=conf_threshold,
             source="data/images",
             save_txt=True,
             save_conf=True,
             name="exp",
         )
 
+    elif model_name == "faster-rcnn":
+        return {"label": "model not done yet!"}
+
     else:
         raise HTTPException(status_code=404, detail="Model Not Found !")
-    
 
     with open(r"runs/detect/exp/labels/image.txt", "r") as f:
         txt = f.read()
 
     json_str = txt_to_json(txt)
     label = json.loads(json_str)
-
     remove_dir("runs/detect/exp")
     return {"label": label}
